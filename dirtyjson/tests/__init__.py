d5c481e37e37f379ca4fe4665920f26b5b3c03be
Scott Maxwell <scott@codecobblers.com>
2015-01-05 15:38:21 -0800

    Fix test suite list

--- dirtyjson/tests/__init__.py
+++ dirtyjson/tests/__init__.py
@@ -16,31 +16,16 @@ def additional_tests(suite=None):
 
 def all_tests_suite():
     suite = unittest.TestLoader().loadTestsFromNames([
-        'dirtyjson.tests.test_binary_encoder',
-        'dirtyjson.tests.test_bigint_as_string',
-        'dirtyjson.tests.test_check_circular',
+        'dirtyjson.tests.test_decimal',
         'dirtyjson.tests.test_decode',
-        'dirtyjson.tests.test_default',
-        'dirtyjson.tests.test_dump',
-        'dirtyjson.tests.test_encode_basestring_ascii',
-        'dirtyjson.tests.test_encode_for_html',
         'dirtyjson.tests.test_errors',
         'dirtyjson.tests.test_fail',
         'dirtyjson.tests.test_float',
-        'dirtyjson.tests.test_indent',
         'dirtyjson.tests.test_pass1',
         'dirtyjson.tests.test_pass2',
         'dirtyjson.tests.test_pass3',
-        'dirtyjson.tests.test_recursion',
         'dirtyjson.tests.test_scanstring',
-        'dirtyjson.tests.test_separators',
-        'dirtyjson.tests.test_speedups',
         'dirtyjson.tests.test_unicode',
-        'dirtyjson.tests.test_decimal',
-        'dirtyjson.tests.test_tuple',
-        'dirtyjson.tests.test_namedtuple',
-        'dirtyjson.tests.test_tool',
-        'dirtyjson.tests.test_for_json',
     ])
     suite = additional_tests(suite)
     return unittest.TestSuite([suite])
